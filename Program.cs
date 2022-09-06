91395b432c07a804d98c7afbc899cb38618c3fe2
pliniopvv <gangss2@Hotmail.com>
2022-09-06 13:24:40 -0300

    finalizando projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("34999999999", "6","46143543433435",4);
+i.Ligar();
+i.InstalarAplicativo("WhatsApp");
+i.ReceberLigacao();
+
+Nokia n = new Nokia("34555555555", "Tijolon", "4354534354354343", 6);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("WhatsApp Black");
